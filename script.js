634f2d4ad79847ef228a75e900e8d0c394da61bd
Jencoll <55255472+Jencoll@users.noreply.github.com>
2021-06-28 16:03:32 -0400

    header + products section started

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const btnBurger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const allItems = document.querySelectorAll('.menu-items');
+
 
 btnBurger.addEventListener('click', () => {
     btnBurger.classList.toggle('active');
+    menu.classList.toggle('active-menu');
 })
+
+allItems.forEach(item => item.addEventListener('click', () => {
+    btnBurger.classList.toggle('active');
+    menu.classList.toggle('active-menu');
+}))

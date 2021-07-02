19bd95837f25609b69fb4a0f24b65dbed4c5b71c
Jencoll <55255472+Jencoll@users.noreply.github.com>
2021-07-02 15:18:53 -0400

    bookmarked active state

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const btnBurger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
 const allItems = document.querySelectorAll('.menu-items');
+const overlay = document.querySelector('.overlay');
+const bookmarkedBtn = document.querySelector('.bookmark-btn');
+const bookmarkTxt = document.querySelector('.bookmark-text');
 
 
 btnBurger.addEventListener('click', () => {
     btnBurger.classList.toggle('active');
     menu.classList.toggle('active-menu');
+    overlay.classList.toggle('active-overlay');
 })
 
 allItems.forEach(item => item.addEventListener('click', () => {
     btnBurger.classList.toggle('active');
     menu.classList.toggle('active-menu');
+    overlay.classList.toggle('active-overlay');
 }))
+
+bookmarkedBtn.addEventListener('mouseup', () => {
+    bookmarkedBtn.classList.toggle('bookmarked');
+
+    if(bookmarkedBtn.classList.contains('bookmarked')){
+        bookmarkTxt.textContent = 'Bookmarked';
+    } else {
+        bookmarkTxt.textContent = 'Bookmark';
+    }
+})
